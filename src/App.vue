8fae24acd1d6545664baecc62d4d4789c7640195
Daniel <daniel0agg@gmail.com>
2026-07-12 12:02:48 -0500

    feat: Integración de Vercel Analytics en App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
+import { inject } from '@vercel/analytics';
+
+inject();
 </script>
 
-  <template>
-    <div class="min-h-screen bg-gray-50 text-gray-900 font-sans transition-colors duration-300">
-      <RouterView />
-    </div>
-  </template>
+<template>
+  <div class="min-h-screen bg-gray-50 text-gray-900 font-sans transition-colors duration-300">
+    <RouterView />
+  </div>
+</template>
 
 <style>
 html {
